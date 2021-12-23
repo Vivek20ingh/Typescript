824a3030ae54e4020576dccebc6232762b4f1b80
vivek20ingh <vs2357766@gmail.com>
2021-12-24 03:48:15 +0530

    update

--- src/Component/Form.tsx
+++ src/Component/Form.tsx
@@ -8,10 +8,8 @@ import Input from '@mui/material/Input';
 import InputLabel from '@mui/material/InputLabel';
 import InputAdornment from '@mui/material/InputAdornment';
 import FormControl from '@mui/material/FormControl';
-import TextField from '@mui/material/TextField';
 import AccountCircle from '@mui/icons-material/AccountCircle';
 import SaveIcon from '@mui/icons-material/Save';
-import LoadingButton from '@mui/lab/LoadingButton';
 import Button from '@mui/material/Button';
 
 
@@ -66,10 +64,9 @@ const Form = () => {
                             </InputAdornment>
                         }
                         value={object.name}
-                        onChange={e => { setobject({ ...object, name: e.target.value }) }}
-
-                    />
+                        onChange={e => { setobject({ ...object, name: e.target.value }) }}/>
                 </FormControl>
+
                 <FormControl variant="standard">
                     <InputLabel htmlFor="input-with-icon-adornment">CITY</InputLabel>
                     <Input
@@ -79,10 +76,9 @@ const Form = () => {
                             </InputAdornment>
                         }
                         value={object.city}
-                        onChange={e => { setobject({ ...object, city: e.target.value }) }}
-
-                    />
+                        onChange={e => { setobject({ ...object, city: e.target.value }) }} />
                 </FormControl>
+
                 <FormControl variant="standard">
                     <InputLabel htmlFor="input-with-icon-adornment">AGE</InputLabel>
                     <Input
@@ -92,20 +88,15 @@ const Form = () => {
                             </InputAdornment>
                         }
                         value={object.age}
-                        onChange={e => { setobject({ ...object, age: e.target.value }) }}
-
-                    />
+                        onChange={e => { setobject({ ...object, age: e.target.value }) }} />
                 </FormControl>
 
                 <div>
                     <Button
-                        color="secondary"
-                        onClick={object.unique_id == null ? (evt: Event) => handleSubmit(evt) : (e: Event) => handleEdit(e)}
-                        startIcon={<SaveIcon />}
-                        variant="contained" >
+                        color="secondary" variant="contained"  startIcon={<SaveIcon />}
+                        onClick={object.unique_id == null ? (evt: Event) => handleSubmit(evt) : (e: Event) => handleEdit(e)} >
                         {object.unique_id == null ? "Submit" : "Update"}
                     </Button>
-
                 </div>
 
             </Box>
@@ -121,26 +112,3 @@ export { Form };
 
 
 
-{/* <form className="split left">
-    <h3>Detail</h3>
-    <div className="form-group">
-        <label>Name</label>
-        <input type="text" className="form-control" placeholder="Enter Name"
-            value={object.name}
-            onChange={e => { setobject({ ...object, name: e.target.value }) }} />
-    </div>
-
-    <div className="form-group" style={{ marginTop: 10 }}>
-        <label >City Name</label>
-        <input type="text" className="form-control" placeholder="Enter City Name"
-            value={object.city}
-            onChange={e => { setobject({ ...object, city: e.target.value }) }} />
-    </div>
-
-    <div className="form-group" style={{ marginTop: 10 }}>
-        <label >Age</label>
-        <input type="text" className="form-control" placeholder="Enter Your Age"
-            value={object.age}
-            onChange={e => { setobject({ ...object, age: e.target.value }); }} />
-    </div>
-    <button onClick={object.unique_id == null ? (evt: Event) => handleSubmit(evt) : (e: Event) => handleEdit(e)} className="btn btn-primary btn-block" style={{ marginTop: 10 }}>{object.unique_id == null ? "Submit" : "Update"}</button> */}
