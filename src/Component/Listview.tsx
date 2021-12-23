9122b7cd6e3dd9aa9886f1eb22a68f23561c0f84
vivek20ingh <vs2357766@gmail.com>
2021-12-23 18:58:00 +0530

    update

--- src/Component/Listview.tsx
+++ src/Component/Listview.tsx
@@ -1,16 +1,22 @@
 import { ListGroup } from "react-bootstrap";
 
-const Listview =({d})=>{
-    return(
-        <ListGroup.Item
-        as="li" className="d-flex justify-content-between align-items-start"react js >
-            <div className="ms-2 me-auto">
-                 <div className="fw-bold">NAME: {d.name}</div>
-                 <div className="fw-bold">CITY: {d.city}</div>
-                  <div className="fw-bold">AGE: {d.age}</div>
-          </div>
-       </ListGroup.Item>
+const Listview = ({ masterData, handleDelete, handleUpdate }) => {
+    return (
+
+        <div className="split right">
+            {masterData.map((d, idx) =>
+                <ListGroup.Item key={d.unique_id}
+                    as="li" className="d-flex justify-content-between align-items-start" react js >
+                    <div className="ms-2 me-auto">
+                        <div className="fw-bold">NAME: {d.name}</div>
+                        <div className="fw-bold">CITY: {d.city}</div>
+                        <div className="fw-bold">AGE:   {d.age}</div>
+                    </div>
+                    <button onClick={() => handleDelete(d.unique_id)}>Delete</button>
+                    <button onClick={() => handleUpdate(d.unique_id)} >Update</button>
+                </ListGroup.Item>)}
+        </div>
     )
 }
 
-export default  Listview;
+export default Listview;
