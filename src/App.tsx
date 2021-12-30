22d9f70876e848d4119ddd635b48e6cf9b5f30b7
vivek20ingh <vs2357766@gmail.com>
2021-12-30 21:18:22 +0530

    form

--- src/App.tsx
+++ src/App.tsx
@@ -1,54 +1,13 @@
 
-import React, { useState } from "react";
-import Form  from "./Component/Form";
-
-import ListContext from './Component/Context/Context'
-import { APPtx } from './Component/Context/Context'
-import shortid from "shortid";
-import * as types from  './Component/Context/Context'
-
+import React from "react";
+import {MyReactComponent} from './Component/Form'
 
 function App() {
-  const [list,setlist] = useState(ListContext.list);
-  const  [ open1, setopen1]= useState(ListContext.open1);
-  const [id,setid1]=useState(ListContext.id)
-
-  const addnote = (object:types.Person):void => {
-    let key: string = shortid.generate();
-    let newobject = object;
-    newobject.unique_id = key;
-    setlist([...list, newobject])
-
-  }
-
-  const updatenote =(object:types.Person):void=>{
-    let newlist = list;
-    let newidx1 = list.findIndex(obj => obj.unique_id === object.unique_id);
-    newlist[newidx1]=object;
-    setlist(list);
-
-  }
-
-  const setpopup =(open:boolean):void=>{
-    setopen1(open)
-  }
-
-  const setid =(idx:string):void=>{
-    setid1(idx);
-  }
-
-  const deletenote =(idx:string):void=>{
-    let newidx = list.findIndex(obj => obj.unique_id === idx);
-    let newlist=list
-    newlist.splice(newidx,1)
-    setlist(newlist)
-  }
 
   return (
     <div className="App">
-      <APPtx.Provider value={{ list, addnote, updatenote ,setpopup,setid,deletenote,open1,id}} >
-        <Form />
-      </APPtx.Provider>
+      <h1>Hello</h1>
+      <MyReactComponent/>
     </div>
   );
 }
--- src/Component/Form.tsx
+++ src/Component/Form.tsx
@@ -1,103 +1,19 @@
-import React, { useState ,useContext} from "react";
-import './Styles.css'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Listview from './Listview'
-import Box from '@mui/material/Box';
-import Input from '@mui/material/Input';
-import InputLabel from '@mui/material/InputLabel';
-import InputAdornment from '@mui/material/InputAdornment';
-import FormControl from '@mui/material/FormControl';
-import AccountCircle from '@mui/icons-material/AccountCircle';
-import SaveIcon from '@mui/icons-material/Save';
-import Button from '@mui/material/Button';
-import * as types from './Context/Context'
-import ConfirmDialog from "./ConfirmDialog";
-import {APPtx}from './Context/Context'
+import {ReactForm} from 'react-forms'
+import React from 'react'
 
-const Form = () => {
-
-    const Person = { unique_id: "", name: "", city: "", age: 0 }
-    const {list ,addnote,updatenote,setpopup,setid}= useContext(APPtx)
-    
-    const [object, setobject] = useState<types.Person>(Person);
-    const handleSubmit = (): void => {
-        addnote(object);
-        setobject(Person)
-    }
-
-    const handleDelete = (idx: string): void => {
-        setpopup(true);
-        setid(idx);
-    }
-
-    const handleUpdate = (idx: string,): void => {
-        let newidx = list.findIndex(obj => obj.unique_id === idx);
-        setobject({ unique_id: idx, name: list[newidx].name, city: list[newidx].city, age: list[newidx].age })
-
-    }
-
-    const handleEdit = (): void => {
-        updatenote(object)
-        setobject(Person)
-    }
-
-    return (
-        <div>
-            <Box sx={{ '& > :not(style)': { m: 1 } }} className="split left">
-                <FormControl variant="standard">
-                    <InputLabel htmlFor="input-with-icon-adornment">Name </InputLabel>
-                    <Input
-                        id="input-with-icon-adornment"
-                        startAdornment={
-                            <InputAdornment position="start">
-                                <AccountCircle />
-                            </InputAdornment>}
-
-                        value={object.name}
-                        onChange={e => { setobject({ ...object, name: e.target.value }) }} />
-                </FormControl>
-
-                <FormControl variant="standard">
-                    <InputLabel htmlFor="input-with-icon-adornment">CITY</InputLabel>
-                    <Input
-                        id="input-with-icon-adornment"
-                        startAdornment={
-                            <InputAdornment position="start">
-                            </InputAdornment>
-                        }
-                        value={object.city}
-                        onChange={e => { setobject({ ...object, city: e.target.value }) }} />
-                </FormControl>
-
-                <FormControl variant="standard">
-                    <InputLabel htmlFor="input-with-icon-adornment">AGE</InputLabel>
-                    <Input
-                        id="input-with-icon-adornment"
-                        startAdornment={
-                            <InputAdornment position="start">
-                            </InputAdornment>
-                        }
-                        value={object.age}
-                        onChange={e => { setobject({ ...object, age: e.target.value }) }} />
-                </FormControl>
-
-                <div>
-                    <Button
-                        color="secondary" variant="contained" startIcon={<SaveIcon />}
-                        onClick={object.unique_id == "" ? () => handleSubmit() : () => handleEdit()} >
-                        {object.unique_id == "" ? "Submit" : "Update"}
-                    </Button>
-                </div>
-            </Box>
-            
-            <ConfirmDialog />
-            <Listview handleDelete={handleDelete} handleUpdate={handleUpdate} />
-        </div>
-    );
+export const MyReactComponent = () => {
+const myConfig = [{
+type : 'text',
+valueKey : 'myTextField',
+fieldProps : { label : 'Sample Text Field' , fullWidth: true } ,
+// fieldProps:{ ...textFieldProps },
+styles : { margin : '0 auto'}
+} ]
+const myInitialValues = [{ myTextField : '' }]
+return ( <div>
+<ReactForm
+config={myConfig}
+// initialValues={myInitialValue}
+onSubmit = {(values : object) => {console.log(values)}}  
+/></div> )
 }
-
-export default Form
-
-
-
-
