da52bc796621bf44209258919ac5b75526606883
vivek20ingh <vs2357766@gmail.com>
2021-12-31 00:57:23 +0530

    form

--- src/Component/Form.tsx
+++ src/Component/Form.tsx
@@ -1,24 +1,25 @@
 import { ReactForm } from 'react-forms'
-import React from 'react'
-import { myconfig ,formValidation, actionConfig} from './myconfig'
+import React, { useState } from 'react'
+import { myconfig, formValidation, actionConfig } from './myconfig'
 
 export const MyReactComponent = () => {
-    // const config = myconfig
-    
-   const myInitialValues = [{ myTextField: 12 }]
-  const pios= [{valueKey : "hs"}]
-    const setid =(idx:string):void=>{
-        console.log(idx) 
-        
-        
-
-      }
-    return (
-        <div>
-            <ReactForm
-                config={myconfig}
-                myInitialValues={myInitialValues}
-                validationSchema = {formValidation}
-                onSubmit={(values: string) => setid(values) }
-            /></div>)
+  // const config = myconfig
+const [myconfig1,setmyconfig1] = useState(myconfig)
+  const myInitialValues = [{ myTextField: 10 }]
+  const pios = [{ valueKey: "hs" }]
+  const setid = (idx: string): void => {
+    console.log(idx)
+    console.log(myconfig)
+    myconfig.values = { ...myconfig, myTextField: 12 };
+    setmyconfig1(myconfig)
+    console.log(myconfig)
+  }
+  return (
+    <div>
+      <ReactForm
+        config={myconfig1}
+        myInitialValues={myInitialValues}
+        validationSchema={formValidation}
+        onSubmit={(values: string) => setid(values)}
+      /></div>)
 }
