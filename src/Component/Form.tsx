7056789cf414a0d0c3eab51c2af9dbbe12c62018
vivek20ingh <vs2357766@gmail.com>
2022-01-06 02:34:52 +0530

    Form

--- src/Component/Form.tsx
+++ src/Component/Form.tsx
@@ -1,99 +1,91 @@
-import React, { useState ,useContext} from "react";
+import React, { useState, useContext } from "react";
+import { ReactForm } from 'react-forms'
 import './Styles.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Listview from './Listview'
-import Box from '@mui/material/Box';
-import Input from '@mui/material/Input';
-import InputLabel from '@mui/material/InputLabel';
-import InputAdornment from '@mui/material/InputAdornment';
-import FormControl from '@mui/material/FormControl';
-import AccountCircle from '@mui/icons-material/AccountCircle';
-import SaveIcon from '@mui/icons-material/Save';
-import Button from '@mui/material/Button';
 import * as types from './Context/Context'
 import ConfirmDialog from "./ConfirmDialog";
-import {APPtx}from './Context/Context'
+import { APPtx } from './Context/Context'
 
 const Form = () => {
 
-    const Person = { unique_id: "", name: "", city: "", age: 0 }
-    const {list ,addnote,updatenote,setpopup,setid}= useContext(APPtx)
-    
-    const [object, setobject] = useState<types.Person>(Person);
-    const handleSubmit = (): void => {
-        addnote(object);
-        setobject(Person)
-    }
+  // type xyz = [{
+  //   type: string,
+  //   valueKey: string,
+  //   fieldProps: {
+  //     label: String,
+  //     fullWidth: Boolean,
+  //   }
 
-    const handleDelete = (idx: string): void => {
-        setpopup(true);
-        setid(idx);
-    }
+  // }]
+  const myConfig = [{
+    type: 'text',
+    valueKey: 'name',
+    fieldProps: { label: 'Sample Text name', fullWidth: true },
+  },
+  {
+    type: 'text',
+    valueKey: 'city',
+    fieldProps: { label: 'Sample Text city', fullWidth: true },
+  },
+  {
+    type: 'text',
+    valueKey: 'age',
+    fieldProps: { label: 'Sample Text age', fullWidth: true },
+  }
+  ]
 
-    const handleUpdate = (idx: string,): void => {
-        let newidx = list.findIndex(obj => obj.unique_id === idx);
-        setobject({ unique_id: idx, name: list[newidx].name, city: list[newidx].city, age: list[newidx].age })
+  const Person = { unique_id: "", name: "", city: "", age: 0 }
+  const { list, addnote, updatenote, setpopup, setid } = useContext(APPtx)
 
-    }
+  const [object, setobject] = useState<types.Person>(Person);
 
-    const handleEdit = (): void => {
-        updatenote(object)
-        setobject(Person)
-    }
+  const handleSubmit = (values: object): void => {
+    object.name=values.name;
+    object.city=values.city;
+    object.age=values.age;
+    // setobject({...object,values})
+    setobject(object);
+    addnote(object);
+    setobject(Person)
+    console.log(object);
+  }
 
-    return (
-        <div>
-            <Box sx={{ '& > :not(style)': { m: 1 } }} className="split left">
-                <FormControl variant="standard">
-                    <InputLabel htmlFor="input-with-icon-adornment">Name </InputLabel>
-                    <Input
-                        id="input-with-icon-adornment"
-                        startAdornment={
-                            <InputAdornment position="start">
-                                <AccountCircle />
-                            </InputAdornment>}
+  const handleDelete = (idx: string): void => {
+    setpopup(true);
+    setid(idx);
+  }
 
-                        value={object.name}
-                        onChange={e => { setobject({ ...object, name: e.target.value }) }} />
-                </FormControl>
+  const handleUpdate = (idx: string,): void => {
+    let newidx = list.findIndex(obj => obj.unique_id === idx);
+    setobject({ unique_id: idx, name: list[newidx].name, city: list[newidx].city, age: list[newidx].age })
 
-                <FormControl variant="standard">
-                    <InputLabel htmlFor="input-with-icon-adornment">CITY</InputLabel>
-                    <Input
-                        id="input-with-icon-adornment"
-                        startAdornment={
-                            <InputAdornment position="start">
-                            </InputAdornment>
-                        }
-                        value={object.city}
-                        onChange={e => { setobject({ ...object, city: e.target.value }) }} />
-                </FormControl>
+  }
 
-                <FormControl variant="standard">
-                    <InputLabel htmlFor="input-with-icon-adornment">AGE</InputLabel>
-                    <Input
-                        id="input-with-icon-adornment"
-                        startAdornment={
-                            <InputAdornment position="start">
-                            </InputAdornment>
-                        }
-                        value={object.age}
-                        onChange={e => { setobject({ ...object, age: e.target.value }) }} />
-                </FormControl>
+  const handleEdit = (): void => {
+    updatenote(object)
+    setobject(Person)
+  }
 
-                <div>
-                    <Button
-                        color="secondary" variant="contained" startIcon={<SaveIcon />}
-                        onClick={object.unique_id == "" ? () => handleSubmit() : () => handleEdit()} >
-                        {object.unique_id == "" ? "Submit" : "Update"}
-                    </Button>
-                </div>
-            </Box>
-            
-            <ConfirmDialog />
-            <Listview handleDelete={handleDelete} handleUpdate={handleUpdate} />
-        </div>
-    );
+  return (
+
+
+    <div className="split left">
+      <ReactForm
+        config={myConfig}
+
+        // initialValues={[{ myTextField: '6' }, { myTextField2: '12' }]}
+        onSubmit={object.unique_id == "" ? (values: object) => handleSubmit(values) : () => handleEdit()} 
+
+      />
+      <ConfirmDialog />
+      <Listview handleDelete={handleDelete} handleUpdate={handleUpdate} />
+    </div>
+
+
+
+
+  );
 }
 
 export default Form
@@ -101,3 +93,5 @@ export default Form
 
 
 
+
+// const myInitialValues = [{ myTextField : 'c' }]
